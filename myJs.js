00f0c487b9da324cd90924e4d29a29c7c6b9422f
jerrymarvis1 <116045895+jerrymarvis1@users.noreply.github.com>
2024-01-25 10:08:00 +0100

    Add files via upload

---
+++ myJs.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function myfunction(){
+    window.location.href = "https://wa.me/message/2YVHGQWQ3SJUO1";
+}
